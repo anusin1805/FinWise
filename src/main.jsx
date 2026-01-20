675173fd81c60d9ef88f1ea411f49a9b45a2438f
anusin1805 <156580126+anusin1805@users.noreply.github.com>
2026-01-20 21:42:47 +0530

    Update main.jsx
    Signed-off-by: anusin1805 <156580126+anusin1805@users.noreply.github.com>

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // This imports the logic you provided
+import App from './App.jsx' // Make sure this matches the new name
 import './style.css';    // This imports your styles
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
